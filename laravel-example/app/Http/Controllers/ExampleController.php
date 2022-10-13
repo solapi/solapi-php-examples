dab0969044dab35aa1dab4bb1bc5ca69e661c7d4
Subin Lee <chandar.sblee@gmail.com>
2022-10-13 18:00:46 +0900

    [Add] add parameters and annotations in get_groups, get_group_messages, get_statistics methods

--- laravel-example/app/Http/Controllers/ExampleController.php
+++ laravel-example/app/Http/Controllers/ExampleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -31,7 +32,7 @@ class ExampleController extends Controller
             $getMessagesRequest->setStartKey($startKey);
         });
 
-        // 데이터 조회 건 수 지정
+        // 데이터 조회 건 수 지정, 최대 500건 까지 가능
         $request->whenHas('limit', function (int $limit) use ($getMessagesRequest) {
             $getMessagesRequest->setLimit($limit);
         });
@@ -78,8 +79,8 @@ class ExampleController extends Controller
             // 필요하다면 일자 검색유형을 선택할 수 있음, CREATED: 생성일 기준, UPDATED: 수정일 기준, 기본값: CREATED
             // $getMessagesRequest->setDateType('UPDATED');
 
-            $startDate = $request->get('startDate');
-            $endDate = $request->get('endDate');
+            $startDate = Carbon::parse($request->get('startDate'))->toIso8601String();
+            $endDate = Carbon::parse($request->get('endDate'))->toIso8601String();
             $getMessagesRequest->setStartDate($startDate)
                 ->setEndDate($endDate);
         }
@@ -88,6 +89,11 @@ class ExampleController extends Controller
         return response()->json($messages);
     }
 
+    /**
+     * 문자 발송(단문 문자, 장문 문자, 사진 문자)
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function send(Request $request): JsonResponse
     {
         $request->validate([
@@ -121,39 +127,101 @@ class ExampleController extends Controller
         }
     }
 
+    /**
+     * 잔액조회
+     * @return JsonResponse
+     */
     public function get_balance(): JsonResponse
     {
         $balance = $this->messageService->getBalance();
         return response()->json($balance);
     }
 
+    /**
+     * 그룹 조회
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function get_groups(Request $request): JsonResponse
     {
-        // TODO: Parameter 설정해야 함
         $getGroupsRequest = new GetGroupsRequest();
+
+        // 페이지네이션을 위한 시작 키 설정, 메시지 조회 시 nextKey로 무한 페이지네이션 가능
+        $request->whenHas('startKey', function (string $startKey) use ($getGroupsRequest) {
+            $getGroupsRequest->startKey = $startKey;
+        });
+
+        // 데이터 조회 건 수 지정, 최대 500건 까지 가능
+        $request->whenHas('limit', function (int $limit) use ($getGroupsRequest) {
+            $getGroupsRequest->limit = $limit;
+        });
+
+        // 날짜로 조회
+        if ($request->has('startDate') && $request->has('endDate')) {
+            $startDate = Carbon::parse($request->get('startDate'))->toIso8601String();
+            $endDate = Carbon::parse($request->get('endDate'))->toIso8601String();
+
+            $getGroupsRequest->startDate = $startDate;
+            $getGroupsRequest->endDate = $endDate;
+        }
+
         $groups = $this->messageService->getGroups($getGroupsRequest);
         return response()->json($groups);
     }
 
+    /**
+     * 그룹 조회
+     * @param string $groupId
+     * @return JsonResponse
+     */
     public function get_group(string $groupId): JsonResponse
     {
-        // TODO: Parameter 설정해야 함
         $group = $this->messageService->getGroup($groupId);
         return response()->json($group);
     }
 
+    /**
+     * 특정 그룹 내 메시지 목록 조회
+     * @param string $groupId
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function get_group_messages(string $groupId, Request $request): JsonResponse
     {
-        // TODO: Parameter 설정해야 함
         $getGroupMessagesRequest = new GetGroupMessagesRequest();
+
+        // 페이지네이션을 위한 시작 키 설정, 메시지 조회 시 nextKey로 무한 페이지네이션 가능
+        $request->whenHas('startKey', function (string $startKey) use ($getGroupMessagesRequest) {
+            $getGroupMessagesRequest->setStartKey($startKey);
+        });
+
+        // 데이터 조회 건 수 지정, 최대 500건 까지 가능
+        $request->whenHas('limit', function (int $limit) use ($getGroupMessagesRequest) {
+            $getGroupMessagesRequest->setLimit($limit);
+        });
+
         $groupMessages = $this->messageService->getGroupMessages($groupId, $getGroupMessagesRequest);
         return response()->json($groupMessages);
     }
 
+    /**
+     * 사용 현황(통계) 조회
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function get_statistics(Request $request): JsonResponse
     {
-        // TODO: Parameter 설정해야 함
         $getStatisticsRequest = new GetStatisticsRequest();
+
+        // 날짜로 조회
+        if ($request->has('startDate') && $request->has('endDate')) {
+            $startDate = Carbon::parse($request->get('startDate'))->toIso8601String();
+            $endDate = Carbon::parse($request->get('endDate'))->toIso8601String();
+
+            $getStatisticsRequest->setStartDate($startDate)
+                ->setEndDate($endDate);
+        }
+
         $statistics = $this->messageService->getStatistics($getStatisticsRequest);
         return response()->json($statistics);
     }
